b80bb4feeec54c9efb39a2495491caddf945fecf
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 16:24:24 +0100

    Another attempt...

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,21 @@
 
 private_key=$1
 github_ref=$2
-echo "$private_key"
-echo "$github_ref"
-cd "$GITHUB_WORKSPACE"
+github_origin=$3
+echo "Ref: $github_ref"
+echo "Origin: $github_origin"
+eval `ssh-agent -t 60 -s`
+echo "$private_key" | ssh-add -
+mkdir -p ~/.ssh/
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git config --local user.email "autoformat@skirt"
+git config --local user.name "skirt-format"
+git clone "$github_origin" repo
+git checkout "$github_ref"
+cd repo
 clang-format-9 -i *.cpp
 if ! git diff --no-ext-diff --quiet --exit-code; then
-  git config --local user.email "autoformat@skirt"
-  git config --local user.name "skirt-format"
   git commit -am "Autoformatting."
-  git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
-  eval `ssh-agent -t 60 -s`
-  echo "$private_key" | ssh-add -
-  mkdir -p ~/.ssh/
-  ssh-keyscan github.com >> ~/.ssh/known_hosts
-  git push origin HEAD:"$github_ref"
-  ssh-agent -k
+  git push origin "$github_ref"
 fi
+ssh-agent -k
