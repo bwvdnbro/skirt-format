99fa1c9e0acee8b346eb360593c580e333d99fbe
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 15:10:43 +0100

    First attempt at auto-committing changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,14 @@
 echo "$GITHUB_WORKSPACE"
 cd "$GITHUB_WORKSPACE"
 clang-format-9 -i *.cpp
-git diff
+if ! git diff --no-ext-diff --quiet --exit-code; then
+  git add .
+  git commit -am "Autoformatting."
+  git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
+  eval `ssh-agent -t 60 -s`
+  echo "$private_key" | ssh-add -
+  mkdir -p ~/.ssh/
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
+  git push
+  ssh-agent -k
+fi
