2eb6aa40916a27795dedc2451223bbc4f01b9d4e
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 17:03:55 +0100

    More changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 private_key=$1
 github_ref=$2
 github_origin=$3
-echo "$INPUT_PRIVATE_KEY" > key.txt
-echo "$INPUT_GITHUB_REF"
-echo "$INPUT_GITHUB_ORIGIN"
-echo "$GITHUB_REF"
-echo "$GITHUB_SHA"
-echo "$GITHUB_REPOSITORY"
-echo "$ACTIONS_RUNTIME_URL"
+echo "INPUT_PRIVATE_KEY: $INPUT_PRIVATE_KEY" > key.txt
+echo "INPUT_GITHUB_REF: $INPUT_GITHUB_REF"
+echo "INPUT_GITHUB_ORIGIN: $INPUT_GITHUB_ORIGIN"
+echo "GITHUB_REF: $GITHUB_REF"
+echo "GITHUB_SHA: $GITHUB_SHA"
+echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
+echo "ACTIONS_RUNTIME_URL: $ACTIONS_RUNTIME_URL"
 wc key.txt
 md5sum key.txt
 echo "Ref: $github_ref"
@@ -17,15 +17,17 @@ echo "Origin: $github_origin"
 eval `ssh-agent -t 60 -s`
 mkdir -p ~/.ssh/
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git config --local user.email "autoformat@skirt"
-git config --local user.name "skirt-format"
 echo "$INPUT_PRIVATE_KEY" | ssh-add -
+echo "TEST SSH:"
 ssh -T git@github.com
+echo "$INPUT_PRIVATE_KEY" | ssh-add -
 git clone git@github.com:"$GITHUB_REPOSITORY".git repo
-git checkout "$github_ref"
 cd repo
+git checkout "$github_ref"
 clang-format-9 -i *.cpp
 if ! git diff --no-ext-diff --quiet --exit-code; then
+  git config --local user.email "autoformat@skirt"
+  git config --local user.name "skirt-format"
   git commit -am "Autoformatting."
   echo "$private_key" | ssh-add -
   git push origin "$github_ref"
