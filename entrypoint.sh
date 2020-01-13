e75b59ea659b0e5d20fe3af9e3aca9119cb2cf7c
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 16:02:47 +0100

    Additional fixes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,17 @@
 
 private_key=$1
 github_ref=$2
-github_origin=$3
 echo "$github_ref"
-echo "$github_origin"
 cd "$GITHUB_WORKSPACE"
 clang-format-9 -i *.cpp
 if ! git diff --no-ext-diff --quiet --exit-code; then
   git config --local user.email "autoformat@skirt"
   git config --local user.name "skirt-format"
-  git checkout "$github_ref"
   git commit -am "Autoformatting."
-  git remote set-url origin "$github_origin"
   eval `ssh-agent -t 60 -s`
   echo "$private_key" | ssh-add -
   mkdir -p ~/.ssh/
   ssh-keyscan github.com >> ~/.ssh/known_hosts
-  git push
+  git push origin HEAD:"$github_ref"
   ssh-agent -k
 fi
