c865184a6f0423134be47ddbc20b7f7b57ed42d4
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 15:14:25 +0100

    Added github identity to action script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-echo "$GITHUB_WORKSPACE"
 cd "$GITHUB_WORKSPACE"
 clang-format-9 -i *.cpp
 if ! git diff --no-ext-diff --quiet --exit-code; then
   git add .
+  git config --local user.email "autoformat@skirt"
+  git config --local user.name "skirt-format"
   git commit -am "Autoformatting."
   git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
   eval `ssh-agent -t 60 -s`
