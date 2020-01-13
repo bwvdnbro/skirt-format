a48e511933a9dee5b1b1c014574cdf1de4042977
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 17:41:31 +0100

    Added status code to action.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,8 @@
 
 cd "$GITHUB_WORKSPACE"
 clang-format-9 -i *.cpp
+if ! git diff --no-ext-diff --quiet --exit-code; then
+  echo ::set-output name=status::"changed"
+else
+  echo ::set-output name=status::"unchanged"
+fi
