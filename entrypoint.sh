d11a43fd78d0b69ca5ff9f698ae3c2688be7b2a5
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 15:34:31 +0100

    Further attempt to auto-commit changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -l
 
+echo "$github_ref"
+echo "$github_origin"
 cd "$GITHUB_WORKSPACE"
 clang-format-9 -i *.cpp
 if ! git diff --no-ext-diff --quiet --exit-code; then
-  git add .
   git config --local user.email "autoformat@skirt"
   git config --local user.name "skirt-format"
+  git checkout "$github_ref"
   git commit -am "Autoformatting."
-  git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
+  git remote set-url origin "$github_origin"
   eval `ssh-agent -t 60 -s`
   echo "$private_key" | ssh-add -
   mkdir -p ~/.ssh/
