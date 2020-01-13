92ec386b378b93e62e7eddae8ee91cc91cdaccd6
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2020-01-13 16:06:20 +0100

    Attempt to fix commit url.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
   git config --local user.email "autoformat@skirt"
   git config --local user.name "skirt-format"
   git commit -am "Autoformatting."
+  git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
   eval `ssh-agent -t 60 -s`
   echo "$private_key" | ssh-add -
   mkdir -p ~/.ssh/
